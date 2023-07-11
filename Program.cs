850b7c9b37d0756d357268464001063f1304ec8a
salman1256@yahoo.com <salman1256@yahoo.com>
2023-07-11 14:05:01 +0530

    Named arguments

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace ConAppMethod
             Emp emp = new Emp();
             
             Console.WriteLine(emp.FullName("Sam", "Dicosta"));
+            Console.WriteLine(emp.FullName("Dicosta", "Sam"));
             Console.ReadKey();
         }
     }
